5d23daf142348f210d6a68cc144ae4dc90670378
Vinicio M. Silva <silva7vm@gmail.com>
2020-06-02 19:48:01 -0300

    Adicione rota para arquivos estaticos

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import { resolve } from 'path';
 
 import routes from './routes';
 import mongodb from './config/mongodb';
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
 
 // Conecta ao MongoDB
 mongodb();
