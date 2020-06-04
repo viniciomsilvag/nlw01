027a53c723b87453fcfd028632cef9e38561ef5d
Vinicio M. Silva <silva7vm@gmail.com>
2020-06-04 09:52:40 -0300

    Constroe o HTML da pagina Home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Home from './pages/Home';
+
 import './App.css';
 
 function App() {
-  return <h1>Hello World</h1>;
+  return <Home />;
 }
 
 export default App;
