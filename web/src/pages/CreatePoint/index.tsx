6f6f201f069a853e3ad95620db22ddc1496f699e
Vinicio M. Silva <silva7vm@gmail.com>
2020-06-04 17:02:55 -0300

    Adiciona opcao de marcar localizacao no mapa

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 
 import api from '../../services/api';
 import apiLocalesIBGE from '../../services/apiLocalesIBGE';
@@ -31,6 +32,10 @@ const CreatePoint = () => {
 
   const [selectedUf, setSelectedUf] = useState('');
   const [selectedCity, setselectedCity] = useState('');
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
+    0,
+    0,
+  ]);
 
   useEffect(() => {
     api.get('items').then(res => {
@@ -66,6 +71,11 @@ const CreatePoint = () => {
   function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
     const city = event.target.value;
     setselectedCity(city);
+  }
+
+  function handleMapClink(event: LeafletMouseEvent) {
+    const { latlng } = event;
+    setSelectedPosition([latlng.lat, latlng.lng]);
   }
 
   return (
@@ -107,12 +117,15 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione um endereço no mapa</span>
           </legend>
-          <Map center={[-5.0906709, -38.3692486]} zoom={15}>
+          <Map
+            center={[-5.0906709, -38.3692486]}
+            zoom={15}
+            onClick={handleMapClink}>
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[-5.0906709, -38.3692486]} />
+            <Marker position={selectedPosition} />
           </Map>
           <div className="field-group">
             <div className="field">
